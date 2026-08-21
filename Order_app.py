7dd442d72411fd048066643cebf0113d807d428c
Pete VanZandt <70492002+pvanzand@users.noreply.github.com>
2026-08-21 09:51:31 -0500

    Update session handling for Snowflake connection

--- Order_app.py
+++ Order_app.py
@@ -19,7 +19,8 @@ st.write (
 Name_on_order = st.text_input('Name on smoothie')
 st.write('The name on your smoothie will be:', Name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
